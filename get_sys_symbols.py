9fc05b92d9b655400e5caf43dcb81533b60bb220
Kevin Postlethwait <k.postlethwait24@gmail.com>
2019-07-22 16:18:24 -0400

    Added handling of env and ldconfig

--- get_sys_symbols.py
+++ get_sys_symbols.py
@@ -1,24 +1,41 @@
-import os
+import os, glob
 import subprocess
 from functools import partial
 
+
+## How to determine what arch the so was made for? (8 zeros or 16 zeros)
 def get_sys_symbols():
     to_ret = None
+    ld_paths = os.environ.get("LD_LIBRARY_PATH")
     if os.path.exists("/usr/lib64"):
-        to_ret = _get_lib_symbols("/usr/lib64")
-        to_ret.union(_get_lib_symbols("/lib64"))
+        to_ret = _get_lib_dir_symbols("/usr/lib64")
+        to_ret.union(_get_lib_dir_symbols("/lib64"))
     elif os.path.exists("/usr/lib32"):
-        to_ret = _get_lib_symbols("/usr/lib32", zeros=8)
-        to_ret.union(_get_lib_symbols("/lib32", zeros=8))
+        to_ret = _get_lib_dir_symbols("/usr/lib32", zeros=8)
+        to_ret.union(_get_lib_dir_symbols("/lib32", zeros=8))
     elif os.path.exists("/usr/lib"):
-        to_ret = _get_lib_symbols("/usr/lib", zeros=8)
-        to_ret.union(_get_lib_symbols("/lib", zeros=8))
+        to_ret = _get_lib_dir_symbols("/usr/lib", zeros=8)
+        to_ret.union(_get_lib_dir_symbols("/lib", zeros=8))
     else:
         raise FileNotFoundError("No usr libraries were found")
 
+    if ld_paths:
+        for path in ld_paths.split(":"):
+            to_ret.union(_get_lib_dir_symbols(path))
+
+    os.chdir("/etc/")
+    with open("/etc/ld.so.conf", "r") as f:
+        for line in f.readlines():
+            paths = glob.glob("ld.so.conf.d/*.conf")
+            for path in paths:
+                with open(path, "r") as conf_file:
+                    for conf_path in conf_file.readlines():
+                        to_ret.union(_get_lib_dir_symbols(conf_path.strip()))
+
     return to_ret
 
-def _get_lib_symbols(root_dir, zeros=16):
+
+def _get_lib_dir_symbols(root_dir, zeros=16):
     to_ret = set({})
     zero_string = "0" * zeros
     command = f"nm -D {root_dir}/*.so* | grep {zero_string}"
